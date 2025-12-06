2b6384554adc6ecb9d7cd7051583159db7028e55
sona.darshan <151994881+sonadarshan99@users.noreply.github.com>
2025-12-06 23:41:05 +0530

    Fix Vercel routing for deep links

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND || "http://localhost:4000",
+  baseURL: process.env.REACT_APP_BACKEND || "https://backend-airtable2.onrender.com",
   withCredentials: true
 });
 export default instance;
